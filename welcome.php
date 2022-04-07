4075025cb551a173e654603b3a396bb7c4a1b9b5
ncd2004c <87205250+ncd2004c@users.noreply.github.com>
2022-04-07 07:57:19 -0500

    Kicks you if not login in

--- welcome.php
+++ welcome.php
@@ -1,14 +1,9 @@
-<!--  I honor Parkland's core values by affirming that I have
-followed all academic integrity guidelines for this work.
-Nolan Decker
-CSC-155DR -->
 <html>
 <head>
 <?php
-
-require("lib/phpfunctions.php");
-
 session_start();
+require("lib/phpfunctions.php");
+checkcredentials();
 ?>
 </head>
 <body>
