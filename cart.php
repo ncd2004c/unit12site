00735868191b702ffee5cff80e40194b557a1d3f
ncd2004c <87205250+ncd2004c@users.noreply.github.com>
2022-04-07 08:00:21 -0500

    Made a Cart page that works
    You can buy or remove all items

--- cart.php
+++ cart.php
@@ -1,7 +1,3 @@
-<!--  I honor Parkland's core values by affirming that I have
-followed all academic integrity guidelines for this work.
-Nolan Decker
-CSC-155DR -->
 <html>
 <head>
 <?php
@@ -9,13 +5,46 @@ CSC-155DR -->
 require("lib/phpfunctions.php");
 
 session_start();
+checkcredentials();
+
+if (!isset($_POST['choice']))
+{
+        // nothing to do
+}
+else if ($_POST['choice'] == 'Make Purchase')
+{
+        echo 'Your card has been billed!';
+        $_SESSION['Golfballs'] = 0;
+        $_SESSION['baseballs'] = 0;
+        $_SESSION['tennisballs'] = 0;
+        $_SESSION['bouncyballs'] = 0;
+}
+else if ($_POST['choice'] == 'Remove Purchase')
+{
+        $_SESSION['Golfballs'] = 0;
+        $_SESSION['baseballs'] = 0;
+        $_SESSION['tennisballs'] = 0;
+        $_SESSION['bouncyballs'] = 0;
+}
 ?>
 </head>
 <body>
 <?php readfile("lib/header.html"); ?>
 <br>
-<b>Shopping Cart Page</b>
+Iteams:
 <br>
+Golf balls: <?php echoSession("Golfballs") ?> <br>
+<br>
+Baseballs: <?php echoSession("baseballs") ?> <br>
+<br>
+Tennis balls: <?php echoSession("tennisballs") ?> <br>
+<br>
+Bouncy balls: <?php echoSession("bouncyballs") ?> <br>
+<br>
+<form method='POST'>
+<input type='submit' name='choice' value='Make Purchase'>
+<input type='submit' name='choice' value='Remove Purchase'>
+</form>
 <?php readfile("lib/footer.html"); ?>
 </body>
 </html>
