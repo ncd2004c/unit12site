67d1c94dd089923912a2c09c77988df05b4ad7f9
ncd2004c <87205250+ncd2004c@users.noreply.github.com>
2022-04-07 08:03:40 -0500

    Item page to buy items
    You buy golf balls by typing how many you want of buy 10 or can remove all golf ball purchases.

--- iteams1.php
+++ iteams1.php
@@ -1,7 +1,3 @@
-<!--  I honor Parkland's core values by affirming that I have
-followed all academic integrity guidelines for this work.
-Nolan Decker
-CSC-155DR -->
 <html>
 <head>
 <?php
@@ -9,12 +5,44 @@ CSC-155DR -->
 require("lib/phpfunctions.php");
 
 session_start();
+checkcredentials();
+$product = 'Golfballs';
+if (!isset($_SESSION['Golfballs']))
+{
+        $_SESSION['Golfballs']=0;
+}
+
+if (!isset($_POST['choice']))
+{
+        // we got here via a link
+}
+else if ($_POST['choice'] == 'Buy')
+{
+        $_SESSION['Golfballs'] += $_POST['Golfballs'];
+}
+else if ($_POST['choice'] == 'Add 10')
+{
+        $_SESSION['Golfballs'] += 10;
+}
+else if ($_POST['choice'] == 'Remove All')
+{
+        $_SESSION['Golfballs'] = 0;
+}
+
 ?>
 </head>
 <body>
 <?php readfile("lib/header.html"); ?>
 <br>
-<b>Iteam Page</b>
+<form method='POST'>
+<input type='number' name=Golfballs value='<?php echoPost("Golfballs");?> '>
+<br>
+<input type='submit' name='choice' value='Buy'>
+<input type='submit' name='choice' value='Add 10'>
+<input type='submit' name='choice' value='Remove All'>
+
+</form>
+Amount: <?php echoSession("Golfballs"); ?>
 <br>
 <?php readfile("lib/footer.html"); ?>
 </body>
