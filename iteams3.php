36000d945c94cc0e79f0c3c8a11fb188d4e6a967
ncd2004c <87205250+ncd2004c@users.noreply.github.com>
2022-04-07 08:08:05 -0500

    Working Item page
    You can buy Tennis balls by typing how many you want or by clicking the buy 10 button. You can remove all tennis balls in cart.

--- iteams3.php
+++ iteams3.php
@@ -1,7 +1,3 @@
-<!--  I honor Parkland's core values by affirming that I have
-followed all academic integrity guidelines for this work.
-Nolan Decker
-CSC-155DR -->
 <html>
 <head>
 <?php
@@ -9,12 +5,44 @@ CSC-155DR -->
 require("lib/phpfunctions.php");
 
 session_start();
+checkcredentials();
+$product = 'tennisballs';
+if (!isset($_SESSION['tennisballs']))
+{
+        $_SESSION['tennisballs']=0;
+}
+
+if (!isset($_POST['choice']))
+{
+        // we got here via a link
+}
+else if ($_POST['choice'] == 'Buy')
+{
+        $_SESSION['tennisballs'] += $_POST['tennisballs'];
+}
+else if ($_POST['choice'] == 'Add 10')
+{
+        $_SESSION['tennisballs'] += 10;
+}
+else if ($_POST['choice'] == 'Remove All')
+{
+        $_SESSION['tennisballs'] = 0;
+}
+
 ?>
 </head>
 <body>
 <?php readfile("lib/header.html"); ?>
 <br>
-<b>Iteam Page</b>
+<form method='POST'>
+<input type='number' name=tennisballs value='<?php echoPost("tennisballs");?> '>
+<br>
+<input type='submit' name='choice' value='Buy'>
+<input type='submit' name='choice' value='Add 10'>
+<input type='submit' name='choice' value='Remove All'>
+
+</form>
+Amount: <?php echoSession("tennisballs"); ?>
 <br>
 <?php readfile("lib/footer.html"); ?>
 </body>
