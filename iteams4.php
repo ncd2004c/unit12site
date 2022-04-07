78732b33708ffcd36bf012aba1db8ad33934adb6
ncd2004c <87205250+ncd2004c@users.noreply.github.com>
2022-04-07 08:10:02 -0500

    Working Item page
    You can buy Bouncy balls by typing how many you want or buy 10 by clicking the buy 10 button. You can also remove all bouncy balls in cart.

--- iteams4.php
+++ iteams4.php
@@ -1,7 +1,3 @@
-<!--  I honor Parkland's core values by affirming that I have
-followed all academic integrity guidelines for this work.
-Nolan Decker
-CSC-155DR -->
 <html>
 <head>
 <?php
@@ -9,12 +5,44 @@ CSC-155DR -->
 require("lib/phpfunctions.php");
 
 session_start();
+checkcredentials();
+$product = 'bouncyballs';
+if (!isset($_SESSION['bouncyballs']))
+{
+        $_SESSION['bouncyballs']=0;
+}
+
+if (!isset($_POST['choice']))
+{
+        // we got here via a link
+}
+else if ($_POST['choice'] == 'Buy')
+{
+        $_SESSION['bouncyballs'] += $_POST['bouncyballs'];
+}
+else if ($_POST['choice'] == 'Add 10')
+{
+        $_SESSION['bouncyballs'] += 10;
+}
+else if ($_POST['choice'] == 'Remove All')
+{
+        $_SESSION['bouncyballs'] = 0;
+}
+
 ?>
 </head>
 <body>
 <?php readfile("lib/header.html"); ?>
 <br>
-<b>Iteam Page</b>
+<form method='POST'>
+<input type='number' name=bouncyballs value='<?php echoPost("bouncyballs");?> '>
+<br>
+<input type='submit' name='choice' value='Buy'>
+<input type='submit' name='choice' value='Add 10'>
+<input type='submit' name='choice' value='Remove All'>
+
+</form>
+Amount: <?php echoSession("bouncyballs"); ?>
 <br>
 <?php readfile("lib/footer.html"); ?>
 </body>
