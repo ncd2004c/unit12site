d370a1276dd9a0c2e1f1e8b54e127ddec990c091
ncd2004c <87205250+ncd2004c@users.noreply.github.com>
2022-04-07 08:05:42 -0500

    Working Item page
    You can buy baseballs by typing how many you want or by clicking the buy 10 button. You can also remove all your baseballs in you cart.

--- iteams2.php
+++ iteams2.php
@@ -1,7 +1,3 @@
-<!--  I honor Parkland's core values by affirming that I have
-followed all academic integrity guidelines for this work.
-Nolan Decker
-CSC-155DR -->
 <html>
 <head>
 <?php
@@ -9,12 +5,44 @@ CSC-155DR -->
 require("lib/phpfunctions.php");
 
 session_start();
+checkcredentials();
+$product = 'baseballs';
+if (!isset($_SESSION['baseballs']))
+{
+        $_SESSION['baseballs']=0;
+}
+
+if (!isset($_POST['choice']))
+{
+        // we got here via a link
+}
+else if ($_POST['choice'] == 'Buy')
+{
+        $_SESSION['baseballs'] += $_POST['baseballs'];
+}
+else if ($_POST['choice'] == 'Add 10')
+{
+        $_SESSION['baseballs'] += 10;
+}
+else if ($_POST['choice'] == 'Remove All')
+{
+        $_SESSION['baseballs'] = 0;
+}
+
 ?>
 </head>
 <body>
 <?php readfile("lib/header.html"); ?>
 <br>
-<b>Iteam Page</b>
+<form method='POST'>
+<input type='number' name=baseballs value='<?php echoPost("baseballs");?> '>
+<br>
+<input type='submit' name='choice' value='Buy'>
+<input type='submit' name='choice' value='Add 10'>
+<input type='submit' name='choice' value='Remove All'>
+
+</form>
+Amount: <?php echoSession("baseballs"); ?>
 <br>
 <?php readfile("lib/footer.html"); ?>
 </body>
