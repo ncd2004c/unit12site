d0cfd286e26955ca69d70d5cdd6db2d54310b9d3
ncd2004c <87205250+ncd2004c@users.noreply.github.com>
2022-04-26 19:20:08 -0500

    Updated the login system

--- loginpage.php
+++ loginpage.php
@@ -12,12 +12,17 @@ function checkLogin($user, $pass)
                 return False;
 }
 
+$user = 'ndecker5';
+$conn = mysqli_connect("localhost",$user,$user,$user);
 
 $message='';
 if (isset($_POST['choice'])) // Have we been here before?
 {
-        if ( checkLogin($_POST['155username'], $_POST['155password']) )
+        $row = lookupUsername($conn,$_POST['155username']);
+        if ($row != 0 && password_verify($_POST['155password'], $row['encrypted_password']))
         {
+
+                $message = 'testing';
                 header('Location: welcome.php');
                 $_SESSION['username'] = $_POST['155username'];
         }
@@ -37,11 +42,13 @@ else
 <br>
 <form method='POST'>
 User <input type='text' name='155username' value='<?php echoPost("155username"); ?>'><br>
-Password <input type='password' name='155password' value='<?php echoPost("155username"); ?>'><br>
+Password <input type='password' name='155password' value='<?php echoPost("155password"); ?>'><br>
 <input type='submit' name='choice' value='Log In'>
 </form>
 <h2><?php echo $message;?></h2>
 <b>Username:username</b>   <b>Password:password</b>
+<br>
+<a href="http://www.csit.parkland.edu/~ndecker5/CSC_155/unit24/newaccount.php">Make New Account</a>
 <br>
 <?php readfile("lib/footer.html"); ?>
 </body>
